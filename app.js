091fb12f69638d3242744a9d4a7dd5e2bd978ed2
Jason Thomas <jasonthomasjat@gmail.com>
2020-12-18 13:34:13 -0800

    added UI elements to add books to and display library

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-let myLibrary = [];
+let myLibrary = [{author: "noth", title: "bites and bits", numberOfPages: 23}];
 
 const libraryUI = document.createElement("div");
 document.body.appendChild(libraryUI);
+
+const addBook = document.createElement("button");
+addBook.textContent = "Add a book to your library."
+
+const showBooks = document.createElement("button");
+showBooks.textContent = "Display your bookshelf."
+libraryUI.append(addBook, showBooks);
+
+libraryUI.addEventListener("click", e=>{ 
+  if(e.target.innerText === "Add a book to your library."){
+    addBook.remove();
+    showBooks.remove();
+    addBookToLibrary();
+  }
+  if(e.target.innerText === "Display your bookshelf."){
+    addBook.remove();
+    showBooks.remove();
+    displayBookShelf();
+  }
+});
+
+
+
+
 
 function Book(author, title, numberOfPages){ 
   this.author = author;
@@ -10,17 +34,68 @@ function Book(author, title, numberOfPages){
 }
 
 function addBookToLibrary(){
+  // const newBookForm = document.createElement("form");
+  // newBookForm.setAttribute("method", "post");
+  // newBookForm.setAttribute("class", "newBookForm");
 
+  // const authorDiv = document.createElement("div");
+  // const authorLabel = document.createElement("label");
+  // authorLabel.setAttribute("for", "author");
+  // authorLabel.innerText = "Author: ";
+  // const author = document.createElement("input");
+  // author.setAttribute("id", "author");
+  // author.setAttribute("name", "author"); 
+  // author.setAttribute("type", "text");
+  // authorDiv.append(authorLabel, author)
+  
+  // const titleDiv = document.createElement("div");
+  // const titleLabel = document.createElement("label");
+  // titleLabel.setAttribute("for", "title");
+  // titleLabel.innerText = "Title: ";
+  // const title = document.createElement("input");
+  // title.setAttribute("id", "title");
+  // title.setAttribute("name", "title"); 
+  // title.setAttribute("type", "text");
+  // titleDiv.append(titleLabel, title);
+
+  // const pagesDiv = document.createElement("div");
+  // const pagesLabel = document.createElement("label");
+  // pagesLabel.setAttribute("for", "pages");
+  // pagesLabel.innerText = "Pages: ";
+  // const pages = document.createElement("input");
+  // pages.setAttribute("id", "pages");
+  // pages.setAttribute("name", "pages"); 
+  // pages.setAttribute("type", "text");
+  // pagesDiv.append(pagesLabel, pages);
+  
+  // const submitBtn = document.createElement("input");
+  // submitBtn.setAttribute("type", "submit");    
+  // submitBtn.setAttribute("value", "Add book to library");
+  // newBookForm.append(authorDiv, titleDiv, pagesDiv, submitBtn);  
+
+  // libraryUI.append(newBookForm);
+
+  // newBookForm.addEventListener("submit", e =>{
+  //   myLibrary.push(new Book(author, title, pages));
+  //   newBookForm.removeEventListener("submit");
+  //   libraryUI.remove(newBookForm);
+  //   libraryUI.append(addBook, showBooks);
+  // });
+  let author = prompt("Enter author of book.");
+  let title = prompt("Enter book title.");
+  let numPages = prompt("Enter number of pages.");
+  myLibrary.push(new Book(author, title, numPages));
+  libraryUI.append(addBook, showBooks);
 }
 
-function displayBookShelf(){
-  const booklist = document.createElement("ul");
-  libraryUI.appendChild(booklist);
+function displayBookShelf(){  
+  const booklist = document.createElement("ul");  
+  libraryUI.append(booklist);
   
   myLibrary.forEach(book => {
     const temp = document.createElement('li');
-    temp.innerText = `${book.title}, by ${book.author} : ${book.numberOfPages} pages.`
-    booklist.appendChild(temp);
+    temp.innerText = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.numberOfPages}`
+    booklist.append(temp);
   });    
 }
 
